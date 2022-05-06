00db32fa660f400abe66589189732da3e2be7540
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-05-06 19:21:34 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/control/KartenEinheitController.java
    #	src/model/Effekt.java
    #	src/model/Einheit.java
    #	src/model/Karte.java
    #	src/model/KarteEinheit.java

--- src/model/Raeume.java
+++ src/model/Raeume.java
@@ -1,6 +1,26 @@
 package model;
 
+import model.ereignisse.Ereignis;
+
 public class Raeume extends Ebenen
 {
+    private Ereignis ereignis = null;
 
+    /**
+     * Konstruktor fuer die Klasse Raume die direkt ein Ereignis in das Attribut ereignis setzt.
+     * @param ereignis die Instanz die in das Attribut ergnis gesetzt werden soll.
+     */
+    public Raeume (Ereignis ereignis)
+    {
+        this.ereignis = ereignis;
+    }
+
+    /**
+     * Methode um das Ereignis aus dem Attribut ereignis zu bekommen.
+     * @return gibt das den Inhalt des Attributes ereignis wieder.
+     */
+    public Ereignis getEreignis ()
+    {
+        return ereignis;
+    }
 }
