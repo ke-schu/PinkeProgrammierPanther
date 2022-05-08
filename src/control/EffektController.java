3641df19786ded013c8a1951efd64b6b4f35e5c4
DieBlaueElise <7crazyhit@gmail.com>
2022-05-08 19:18:32 +0200

    moving classes

--- src/control/EffektController.java
+++ src/control/EffektController.java
@@ -4,11 +4,11 @@ import model.*;
 
 public class EffektController
 {
-    public void effektAusloesen(Einheit ausloeser, Einheit ziel)
+    public void effektAusloesen(Einheit ausloeser, Einheit ziel,Spielfeld feld)
     {
         switch (ausloeser.getEffekt())
         {
-            case LETZTEWORTE: letzteworte(ausloeser, ziel); break;
+            case LETZTEWORTE: letzteworte(ausloeser, ziel, feld); break;
         }
 
     }
@@ -17,15 +17,50 @@ public class EffektController
         ziel.schadenNehmen(schaden);
     }
 
-    public static void letzteworte (Einheit ausfuehrer, Einheit ziel)
+    public static void letzteworte (Einheit ausloeser, Einheit ziel, Spielfeld feld)
     {
-        if (ausfuehrer.getLebenspunkte() == 0);
+        if (ausloeser.getLebenspunkte() == 0);
         {
-            zurueckwerfen(ziel);
+            zurueckwerfen(ausloeser, feld);
         }
     }
-    public static void zurueckwerfen (Einheit ziel)
+    public static void zurueckwerfen (Einheit ausloeser, Spielfeld feld)
     {
+        Einheit zieloben = feld.getSpielfeldplatz(ausloeser.getPosition_x(), ausloeser.getPosition_y()-1);
+        Einheit platzoben = feld.getSpielfeldplatz(ausloeser.getPosition_x(), ausloeser.getPosition_y()-2);
 
+        Einheit zielunten = feld.getSpielfeldplatz(ausloeser.getPosition_x(), ausloeser.getPosition_y()+1);
+        Einheit platzunten = feld.getSpielfeldplatz(ausloeser.getPosition_x(), ausloeser.getPosition_y()+2);
+
+        Einheit ziellinks  = feld.getSpielfeldplatz(ausloeser.getPosition_x()-1, ausloeser.getPosition_y());
+        Einheit platzlinks = feld.getSpielfeldplatz(ausloeser.getPosition_x()-2, ausloeser.getPosition_y());
+
+        Einheit zielrechts  = feld.getSpielfeldplatz(ausloeser.getPosition_x()+1, ausloeser.getPosition_y());
+        Einheit platzrechts = feld.getSpielfeldplatz(ausloeser.getPosition_x()+2, ausloeser.getPosition_y());
+
+        if (zieloben != null && platzoben == null)
+        {
+            feld.einheiteinsetzten(ausloeser.getPosition_x(), ausloeser.getPosition_y()-2, zieloben);
+            feld.einheitloeschen(ausloeser.getPosition_x(), ausloeser.getPosition_y()-1);
+        }
+
+        if (zielunten != null && platzoben == null)
+        {
+            feld.einheiteinsetzten(ausloeser.getPosition_x(), ausloeser.getPosition_y()+2, zieloben);
+            feld.einheitloeschen(ausloeser.getPosition_x(), ausloeser.getPosition_y()+1);
+        }
+
+        if (ziellinks != null && platzoben == null)
+        {
+            feld.einheiteinsetzten(ausloeser.getPosition_x() - 2, ausloeser.getPosition_y(), zieloben);
+            feld.einheitloeschen(ausloeser.getPosition_x() - 1, ausloeser.getPosition_y());
+        }
+
+        if (zielrechts != null && platzoben == null)
+        {
+            feld.einheiteinsetzten(ausloeser.getPosition_x() + 2, ausloeser.getPosition_y(), zieloben);
+            feld.einheitloeschen(ausloeser.getPosition_x() + 1, ausloeser.getPosition_y());
+        }
     }
+
 }
