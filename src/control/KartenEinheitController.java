be6f9f17a3131845f2879b56aa5d193e0f1335dc
DieBlaueElise <7crazyhit@gmail.com>
2022-05-09 11:02:04 +0200

    moving classes

--- src/control/KartenEinheitController.java
+++ src/control/KartenEinheitController.java
@@ -7,9 +7,11 @@ public class KartenEinheitController
     public static void beschwoeren (KarteEinheit karte, Spielfeld spielfeld, int x, int y)
     {
         //benachbarte einheit abfragen nicht vergessen
+        int zug = RundenController.getZugzeahler();
+        if (zug % 2 == 0))
         if (spielfeld.getSpielfeld()[x][y] == null)
         {
-            Einheit meineeinheit = new Einheit(karte);
+            Einheit meineeinheit = new Einheit(karte,);
             spielfeld.einheiteinsetzten(x,y,meineeinheit);
             meineeinheit.setPosition_x(x);
             meineeinheit.setPosition_y(y);
