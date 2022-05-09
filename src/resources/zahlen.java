9da07910de1f98b3a839b33c5f4505f6a9913fe7
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-05-09 10:20:11 +0200

    Zahlen im Interface bis 10 eingetragen

--- src/resources/zahlen.java
+++ src/resources/zahlen.java
@@ -2,4 +2,15 @@ package resources;
 
 public interface zahlen
 {
+    int ZAHL_0 = 0;
+    int ZAHL_1 = 1;
+    int ZAHL_2 = 2;
+    int ZAHL_3 = 3;
+    int ZAHL_4 = 4;
+    int ZAHL_5 = 5;
+    int ZAHL_6 = 6;
+    int ZAHL_7 = 7;
+    int ZAHL_8 = 8;
+    int ZAHL_9 = 9;
+    int ZAHL_10 = 10;
 }
