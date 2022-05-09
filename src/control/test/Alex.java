e6612a033e5d4736eb04252ad6308356169a841b
Keno Schumann <kejuhi79@gmail.com>
2022-05-09 19:02:39 +0200

    "Test.java" gelöscht und in Alex' Klasse verschoben

--- src/control/test/Alex.java
+++ src/control/test/Alex.java
@@ -4,8 +4,8 @@ import control.EffektController;
 import control.KartenEinheitController;
 import exceptions.SpielfeldDimensionGleichNullException;
 import exceptions.SpielfeldNichtQuadratischException;
-import model.KarteEinheit;
-import model.Spielfeld;
+import model.*;
+
 import static model.KartenEinheitType.*;
 
 public class Alex
@@ -20,6 +20,37 @@ public class Alex
         {
             System.out.println(e.getMessage());
         }
+        erstelleKartenHand();
+    }
+
+    private static void erstelleKartenHand()
+    {
+        Karte karteeins = new KarteEinheit(EricKarte);
+        Karte kartezwei = new KarteEinheit(KennyKarte);
+        Karte kartedrei = new KarteEinheit(KyleKarte);
+        Karte kartevier = new KarteEinheit(KyleKarte);
+        Karte kartefuenf = new KarteEinheit(StanKarte);
+
+        KartenDeck meindeck = new KartenDeck("Test");
+        Kartenhand meinehand = new Kartenhand();
+
+
+        meindeck.push(karteeins);
+        meindeck.push(kartezwei);
+        meindeck.push(kartedrei);
+        meindeck.push(kartevier);
+        meindeck.push(kartefuenf);
+
+        System.out.println("Der Stack ist "+meindeck.size()+" Elemente groß.");
+
+        meinehand.handziehen(meindeck);
+
+        System.out.println("habe eine hand gezogen, jetzt ist mein deck "+meindeck.size()+" Elemente groß.");
+        meinehand.karteablegen(1,meindeck);
+        System.out.println("habe eine karte abgelegt jetzt ist mein deck wieder  "+meindeck.size()+" Elemente groß.");
+
+        meinehand.handablegen(meindeck);
+        System.out.println("habe meine hand abgelegt jetzt ist mein deck wieder  "+meindeck.size()+" Elemente groß.");
     }
 
     private static void erstelleSpielfeld () throws SpielfeldNichtQuadratischException, SpielfeldDimensionGleichNullException
