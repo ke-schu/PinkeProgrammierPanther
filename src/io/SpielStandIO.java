aae5473011072dfbf5b28b6895158b9967840d4e
Keno Schumann <kejuhi79@gmail.com>
2022-05-12 19:54:53 +0200

    Kommentare in SpielStandIO

--- src/io/SpielStandIO.java
+++ src/io/SpielStandIO.java
@@ -13,18 +13,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import static resources.Strings.*;
 
+/**
+ * Diese Klasse beinhaltet das Lesen und Schreiben von Spielständen aus bzw. in Dateien.
+ */
 public class SpielStandIO
 {
     private static Gson meinGson;
     private static GsonBuilder meinGsonBuilder = new GsonBuilder();
     private final static File datei = new File(SPIELSTAND_PFAD);
 
+    /**
+     * Serialisiert den Spielstand ins Json-Format.
+     * @param stand der Spielstand
+     * @return einen String im Json-Format
+     */
     private static String serialisieren (SpielStand stand)
     {
         meinGson = meinGsonBuilder.setPrettyPrinting().create();
         return meinGson.toJson(stand);
     }
 
+    /**
+     * Schreibt einen Spielstand serialisiert in die Spielstand-Datei.
+     * @param stand der Spielstand
+     * @throws IOException wenn ein Fehler im Schreiben auftritt.
+     */
     public static void schreibeDatei (SpielStand stand) throws IOException
     {
         if (datei.createNewFile())
@@ -40,13 +53,24 @@ public class SpielStandIO
         verfasser.close();
     }
 
+    /**
+     * Deserialisiert einen im Json-Format vorliegenden String in einen Spielstand.
+     * @param jsonStand die Zeichenkette
+     * @return den Spielstand
+     * @throws JsonSyntaxException wenn die Formatierung nicht mit der Json-Formatierung übereinstimmt.
+     */
     private static SpielStand deserialisieren (String jsonStand) throws JsonSyntaxException
     {
         meinGson = meinGsonBuilder.create();
         return meinGson.fromJson(jsonStand, SpielStand.class);
     }
 
-    public static SpielStand leseDatei () throws IOException, KartenDeckFehlerhaftException
+    /**
+     * Liest die Charaktere-Datei ein und gibt einen Stapel aus Charakteren deserialisiert zurück.
+     * @return den Charakter-Stapel
+     * @throws IOException wenn ein Fehler beim Einlesen auftritt.
+     */
+    public static SpielStand leseDatei () throws IOException
     {
         Path path = Paths.get(datei.toURI());
         String content = Files.readString(path);
@@ -55,7 +79,7 @@ public class SpielStandIO
         {
             return new SpielStand(deserialisieren(content));
         }
-        catch (JsonSyntaxException e)
+        catch (JsonSyntaxException | KartenDeckFehlerhaftException e)
         {
             throw new IOException(JSON_FORMAT_FEHLERHAFT_INFO);
         }
