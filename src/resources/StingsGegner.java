3f1c6e86f7f23e2c71c478bf820d50a74a338945
Adrian <aruholt@stud.hs-bremen.de>
2022-05-12 18:36:02 +0200

    ar

---
+++ src/resources/StingsGegner.java
@@ -0,0 +1,28 @@
+package resources;
+
+/**
+ * Hier befinden sich Strings, welche bei der Erstellung von Gegnern genutzt.
+ */
+public interface StingsGegner
+{
+    //Namen und Beschreibungen für die Gegner der Ebenen
+    String GENERISCHER_GEGNER_NAME ="Gegner";
+    String GENERISCHER_GEGNER_BESCHREIBUNG ="Hallo, ich bin ein Gegner!";
+
+
+    //Namen und Beschreibungen für die Bosse der Ebenen
+    String BOSS_ERSTES_LEVEL_NAME = "Boss 1";
+    String BOSS_ERSTES_LEVEL_BESCHREIBUNG = "Das ist Boss 1";
+    String BOSS_ZWEITES_LEVEL_NAME = "Boss 2";
+    String BOSS_ZWEITES_LEVEL_BESCHREIBUNG = "Das ist Boss 2";
+    String BOSS_DRITTES_LEVEL_NAME = "Boss 3";
+    String BOSS_DRITTES_LEVEL_BESCHREIBUNG = "Das ist Boss 3";
+    String BOSS_VIERTES_LEVEL_NAME = "Boss 4";
+    String BOSS_VIERTES_LEVEL_BESCHREIBUNG = "Das ist Boss 4";
+    String BOSS_FUENFTES_LEVEL_NAME = "Boss 5";
+    String BOSS_FUENFTES_LEVEL_BESCHREIBUNG = "Das ist Boss 5";
+    String BOSS_SECHSTES_LEVEL_NAME = "Boss 6";
+    String BOSS_SECHSTES_LEVEL_BESCHREIBUNG = "Das ist Boss 6";
+    String BOSS_SIEBTES_LEVEL_NAME = "Boss 7";
+    String BOSS_SIEBTES_LEVEL_BESCHREIBUNG = "Das ist Boss 7";
+}
