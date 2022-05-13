ae4c60533d86448e41a577b5a05502ccfc875d0e
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-05-13 21:12:36 +0200

    Methoden für das Angreifen von Einheiten und Zauber

--- src/control/EinheitenController.java
+++ src/control/EinheitenController.java
@@ -25,7 +25,7 @@ public class EinheitenController
         boolean selbeZeile = false;
         boolean selbeSpalte = false;
 
-        if (spielfeld.getSpielfeldplatz(ziel_x, ziel_y)== null)
+        if (spielfeld.getSpielfeldplatz(ziel_x, ziel_y) == null)
         {
             int beweglichkeit = einheit.getBeweglichkeit();
             int distanz = Math.abs((ziel_x - start_x) - (ziel_y - start_y));
@@ -71,6 +71,67 @@ public class EinheitenController
                 spielfeld.einheiteinsetzten(start_x, start_y, null);
                 einheit.setBeweglichkeit(einheit.getBeweglichkeit() - ZAHL_1);
             }
+        }
+    }
+
+    /**
+     * Mit dieser Methode kann mit einer auf dem Spielfeld platzierten Einheit eine weitere angegriffen werden.
+     * @param spielfeld Das Spielfeld auf dem sich beide Einheiten befinden.
+     * @param angreifer Einheit, welche angreift.
+     * @param verteidiger Einheit, welche angegriffen wird.
+     */
+    public static void einheitenAngreifenMitEinheiten (SpielFeld spielfeld, KarteEinheit angreifer, KarteEinheit verteidiger)
+    {
+        boolean zielErreichbarInX = false;
+        boolean zielErreichbarInY = false;
+        boolean selbeZeile = false;
+        boolean selbeSpalte = false;
+
+        selbeZeile = (angreifer.getPosition_x() == verteidiger.getPosition_x());
+        selbeSpalte = (angreifer.getPosition_y() == verteidiger.getPosition_y());
+
+        zielErreichbarInX = ((verteidiger.getPosition_x() <= angreifer.getPosition_x() + angreifer.getReichweite()) || (verteidiger.getPosition_x() >= angreifer.getPosition_x() - angreifer.getReichweite()));
+        zielErreichbarInY = ((verteidiger.getPosition_y() <= angreifer.getPosition_y() + angreifer.getReichweite()) || (verteidiger.getPosition_y() >= angreifer.getPosition_y() - angreifer.getReichweite()));
+
+
+
+        if ((zielErreichbarInX && selbeSpalte) || (zielErreichbarInY && selbeZeile))
+        {
+            berechneSchaden(verteidiger, angreifer.getMacht());
+        }
+    }
+
+    /**
+     * Mit diese Methode kann eine Instanz der Klasse KarteZauber auf eine Instanz der Klasse KarteEinheit innerhalb des Spielfeldes angewendet werden.
+     * Die Macht von KarteZauber wird mit den Lebenspunkten von KarteEinheit verechnet.
+     * @param spielfeld Spiefeld auf dem sich die Instanz von KarteEinheit befindet.
+     * @param zauber Instanz von KarteZauber, welche benutzt werden soll.
+     * @param verteidiger Instanz von KarteEinheit, welche angegriffen wird.
+     */
+    public static void einheitenAngreifenMitKarteZauber (SpielFeld spielfeld, KarteZauber zauber, KarteEinheit verteidiger)
+    {
+        {
+            berechneSchaden(verteidiger, zauber.getMacht());
+        }
+    }
+
+    /**
+     * Methode um Schaden mit den Lebens- und Verteidigungswerten einer Instanz von KarteEinheit zu verrechnen.
+     * @param verteidiger Instanz von KarteEinheit mit dessen Verteidigungswerten ein Schadenswert verrechnet werden soll.
+     * @param schadensWert der Schadenwert, welcher mit dem Ziel verrechnet werden soll.
+     */
+    private static void berechneSchaden (KarteEinheit verteidiger, int schadensWert)
+    {
+        int differenzDefAtk = verteidiger.getVerteidigung() - schadensWert;
+
+        if (verteidiger.getSchild() > ZAHL_0)
+        {
+            verteidiger.setSchild(verteidiger.getSchild() - ZAHL_1);
+
+        }
+        if (differenzDefAtk < ZAHL_0)
+        {
+            verteidiger.setLebenspunkte(verteidiger.getLebenspunkte() - differenzDefAtk);
         }
     }
 }
--- src/model/KarteZauber.java
+++ src/model/KarteZauber.java
@@ -2,8 +2,28 @@ package model;
 
 public class KarteZauber extends Karte
 {
+    private int macht;
+
     public KarteZauber (String name, int level)
     {
         super(name, level);
+    }
+
+    /**
+     * Methoden, welche den Wert des Attributs macht liefert.
+     * @return gibt den Wert macht als Int wieder.
+     */
+    public int getMacht()
+    {
+        return macht;
+    }
+
+    /**
+     * Methode die einen Int-Wert in das Attribut macht setzt.
+     * @param macht Int-Wert, welcher in das Attribut macht gesetzt wird.
+     */
+    public void setMacht(int macht)
+    {
+        this.macht = macht;
     }
 }
