57e86881042d80015ca34492c331a2b3c7c60cc8
Keno Schumann <kejuhi79@gmail.com>
2022-05-16 18:01:54 +0200

    SpielStand Kommentare

--- src/model/SpielStand.java
+++ src/model/SpielStand.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 
 import static resources.Strings.*;
 
+/**
+ * Modelliert einen Spielstand, mit den abzuspeichernden Attributen
+ */
 public class SpielStand
 {
     private int gold;
@@ -18,6 +21,14 @@ public class SpielStand
     private transient KartenDeck spieldeckGegner;
     private transient Ebene aktuelleEbene;
 
+    /**
+     * Konstruiert einen neuen Spielstand mit den notwendigen Attributen
+     * @param gold das Guthaben des Spielers
+     * @param erfahrungspunkte die gesammelten Erfahrungspunkte
+     * @param spieler der kämpfende Spieler
+     * @throws KartenDeckFehlerhaftException wenn die Kartendecks des Spielers oder des Gegners Fehler enthalten
+     * @throws IOException wenn die Spielstanddatei nur falsch oder gar nicht gelesen werden kann
+     */
     public SpielStand(int gold, int erfahrungspunkte, Spieler spieler) throws KartenDeckFehlerhaftException, IOException
     {
         this.gold = gold;
@@ -28,6 +39,13 @@ public class SpielStand
         this.aktuelleEbene = EbeneIO.leseDatei(new File(AKTUELLE_EBENE_PFAD));
     }
 
+    /**
+     * Konstruiert einen neuen Spielstand mit einem bestehenden Spielstand, um die nicht abgespeicherten
+     * Kartendecks und Ebenen aus den anderen json-Dateien in die Instanz zu binden.
+     * @param stand der Spielstand
+     * @throws KartenDeckFehlerhaftException wenn die Kartendecks des Spielers oder des Gegners Fehler enthalten
+     * @throws IOException wenn die Spielstanddatei nur falsch oder gar nicht gelesen werden kann
+     */
     public SpielStand (SpielStand stand) throws KartenDeckFehlerhaftException, IOException
     {
         this(stand.getGold(), stand.getErfahrungspunkte(), stand.getSpieler());
@@ -36,58 +54,84 @@ public class SpielStand
         this.aktuelleEbene = EbeneIO.leseDatei(new File(AKTUELLE_EBENE_PFAD));
     }
 
+    /**
+     * Gibt die aktuelle Spielebene wieder
+     * @return die Spielebene
+     */
     public Ebene getAktuelleEbene()
     {
         return aktuelleEbene;
     }
 
+    /**
+     * Gibt das Gold des Spielstandes wieder
+     * @return das Gold
+     */
     public int getGold()
     {
         return gold;
     }
 
+    /**
+     * Setzt das Gold des Spielstandes auf einen neuen Wert
+     * @param gold das neue Guthaben
+     */
     public void setGold(int gold)
     {
         this.gold = gold;
     }
 
+    /**
+     * Gibt die Erfahrungspunkte des Spielstandes wieder
+     * @return die Erfahrungspunkte
+     */
     public int getErfahrungspunkte()
     {
         return erfahrungspunkte;
     }
 
+    /**
+     * Setzt die Erfahrungspunkte des Spielstandes auf einen neuen Wert
+     * @param erfahrungspunkte die neuen Erfahrungspunkte
+     */
     public void setErfahrungspunkte(int erfahrungspunkte)
     {
         this.erfahrungspunkte = erfahrungspunkte;
     }
 
+    /**
+     * Gibt den Spieler wieder
+     * @return der Spieler
+     */
     public Spieler getSpieler()
     {
         return spieler;
     }
 
+    /**
+     * Setzt den Spieler des Spielstandes neu
+     * @param spieler der Spieler
+     */
     public void setSpieler(Spieler spieler)
     {
         this.spieler = spieler;
     }
 
+    /**
+     * Gibt das Spieldeck des Spielers wieder
+     * @return das Kartendeck des Spielers
+     */
     public KartenDeck getSpieldeckSpieler()
     {
         return spieldeckSpieler;
     }
 
-    public void setSpieldeckSpieler(KartenDeck spieldeckSpieler)
-    {
-        this.spieldeckSpieler = spieldeckSpieler;
-    }
-
+    /**
+     * Gibt das Spieldeck des Gegners wieder
+     * @return das Kartendeck des Gegners
+     */
     public KartenDeck getSpieldeckGegner()
     {
         return spieldeckGegner;
-    }
-
-    public void setSpieldeckGegner(KartenDeck spieldeckGegner)
-    {
-        this.spieldeckGegner = spieldeckGegner;
     }
 }
