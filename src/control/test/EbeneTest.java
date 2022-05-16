304c7d41710938d3ac3bb41737b08d7dbfc389db
HenrikJ00 <hjanovsky@stud.hs-bremen.de>
2022-05-16 12:48:30 +0200

    Interface StringsEreignisse hinzugefügt und Magic Strings ersetzt.

--- src/control/test/EbeneTest.java
+++ src/control/test/EbeneTest.java
@@ -8,9 +8,13 @@ import model.*;
 import model.ereignisse.*;
 import resources.Effekte;
 import resources.Einheiten;
+
 import java.io.File;
 import java.io.IOException;
+
 import static resources.Strings.EBENE_TEST_PFAD;
+import static resources.StringsEreignisse.*;
+import static resources.StringsGegner.GENERISCHER_GEGNER_NAME;
 import static resources.Zahlen.*;
 
 /**
@@ -20,47 +24,38 @@ public class EbeneTest
 {
     /**
      * Methode um eine Instanz von Gegenspieler zu erstellen, welche für das erstellen von Gegnern notwendig ist.
+     *
      * @return gibt eine Instanz von Gegenspieler zurück.
      */
     private static Gegenspieler erstelleGegenspieler ()
     {
-        return new Gegenspieler("Test_Gegner",
-                ZAHL_10,
-                Einheiten.NAHKAEMPFER,
-                ZAHL_4,
-                ZAHL_10,
-                ZAHL_1,
-                ZAHL_2,
-                ZAHL_1,
-                ZAHL_2,
-                Effekte.ZURUECKWERFEN,
-                Effekte.LETZTEWORTE,
-                ZAHL_10);
+        return new Gegenspieler(GENERISCHER_GEGNER_NAME, ZAHL_10, Einheiten.FERNKAEMPFER, ZAHL_1, ZAHL_1, ZAHL_1, ZAHL_1, ZAHL_1,
+                                ZAHL_1, Effekte.LETZTEWORTE, Effekte.ZURUECKWERFEN, ZAHL_10);
     }
 
     /**
-     * Diese Methode testet alle grundlegenden Funktionen einer Ebene.
-     * Es wird eine Ebene und Raeume erstellt, welche mit Ereignissen gefüllt sind.
-     * Anschließend wird die Instanz in die EbeneTest.json geschrieben und anschließend aus dieser
-     * in eine neue Instanz von Ebene geschrieben.
-     * Hiernach durchläuft eine Instanz der Klasse SpielfigurEbene diese Ebene und durchquert alle Ereignisse einmal.
+     * Diese Methode testet alle grundlegenden Funktionen einer Ebene. Es wird eine Ebene und Raeume erstellt, welche
+     * mit Ereignissen gefüllt sind. Anschließend wird die Instanz in die EbeneTest.json geschrieben und anschließend
+     * aus dieser in eine neue Instanz von Ebene geschrieben. Hiernach durchläuft eine Instanz der Klasse
+     * SpielfigurEbene diese Ebene und durchquert alle Ereignisse einmal.
      */
-    public static void testeEbene()
+    public static void testeEbene ()
     {
         Ebene testEbene1 = new Ebene(ZAHL_9, ZAHL_9);
-        Ebene testEbene2 = new Ebene(ZAHL_0,ZAHL_0);
+        Ebene testEbene2 = new Ebene(ZAHL_0, ZAHL_0);
         SpielStand spielStand = null;
 
-        LeererRaum leererRaum1 = new LeererRaum("Startraum", "Hier kann man starten.");
-        LeererRaum leererRaum2 = new LeererRaum("Leerer Raum", "Hier kann man leeren.");
-        Haendler haendler = new Haendler("Haendler", "Hier kann man handeln.");
-        Schmied schmied = new Schmied("Schmied", "Hier kann man schmieden.");
-        Tempel tempel = new Tempel("Tempel", "Hier kann tempeln.");
-        Heiler heiler = new Heiler("Heiler", "Hier kann man heilern.");
-        ZufallsEreignis zufallsEreignis = new ZufallsEreignis("Zufallsereignis", "Hier kann man zufalls ereignissen.",false);
-        Truhe truhe = new Truhe("Truhe", "Hier kann man truhen.", false);
-        Gegner gegner = new Gegner("Gegner", "Hier kann man gegnern.", erstelleGegenspieler(), new SpielFeld());
-        Treppe treppe = new Treppe("Treppe", "Hier kann treppen.");
+        LeererRaum leererRaum1 = new LeererRaum(START_RAUM_NAME, START_RAUM_BESCHREIBUNG);
+        LeererRaum leererRaum2 = new LeererRaum(LEERER_RAUM_NAME, LEERER_RAUM_BESCHREIBUNG);
+        Haendler haendler = new Haendler(HAENDLER_NAME, HAENDLER_BESCHREIBUNG);
+        Schmied schmied = new Schmied(SCHMIED_NAME, SCHMIED_BESCHREIBUNG);
+        Tempel tempel = new Tempel(TEMPEL_NAME, TEMPEL_BESCHREIBUNG);
+        Heiler heiler = new Heiler(HEILER_NAME, HEILER_BESCHREIBUNG);
+        ZufallsEreignis zufallsEreignis =
+                new ZufallsEreignis(ZUFALLS_EREIGNIS_NAME, ZUFALLS_EREIGNIS_BESCHREIBUNG, false);
+        Truhe truhe = new Truhe(TRUHE_NAME, TRUHE_BESCHREIBUNG, false);
+        Gegner gegner = new Gegner(GEGNER_NAME, GEGNER_BESCHREIBUNG, erstelleGegenspieler(), new SpielFeld());
+        Treppe treppe = new Treppe(TREPPE_NAME, TREPPE_BESCHREIBUNG);
 
         Raum startRaum = new Raum(leererRaum1);
         Raum leererRaum = new Raum(leererRaum2);
@@ -73,16 +68,16 @@ public class EbeneTest
         Raum gegnerRaum = new Raum(gegner);
         Raum treppenRaum = new Raum(treppe);
 
-        testEbene1.raumEinsetzen(ZAHL_4,ZAHL_4,startRaum);
-        testEbene1.raumEinsetzen(ZAHL_4,ZAHL_3,leererRaum);
-        testEbene1.raumEinsetzen(ZAHL_4,ZAHL_2,haendlerRaum);
-        testEbene1.raumEinsetzen(ZAHL_4,ZAHL_1,schmiedRaum);
-        testEbene1.raumEinsetzen(ZAHL_4,ZAHL_0,tempelRaum);
-        testEbene1.raumEinsetzen(ZAHL_5,ZAHL_0,heilerRaum);
-        testEbene1.raumEinsetzen(ZAHL_6,ZAHL_0,zufallsRaum);
-        testEbene1.raumEinsetzen(ZAHL_7,ZAHL_0,truhenRaum);
-        testEbene1.raumEinsetzen(ZAHL_8,ZAHL_0,gegnerRaum);
-        testEbene1.raumEinsetzen(ZAHL_8,ZAHL_1,treppenRaum);
+        testEbene1.raumEinsetzen(ZAHL_4, ZAHL_4, startRaum);
+        testEbene1.raumEinsetzen(ZAHL_4, ZAHL_3, leererRaum);
+        testEbene1.raumEinsetzen(ZAHL_4, ZAHL_2, haendlerRaum);
+        testEbene1.raumEinsetzen(ZAHL_4, ZAHL_1, schmiedRaum);
+        testEbene1.raumEinsetzen(ZAHL_4, ZAHL_0, tempelRaum);
+        testEbene1.raumEinsetzen(ZAHL_5, ZAHL_0, heilerRaum);
+        testEbene1.raumEinsetzen(ZAHL_6, ZAHL_0, zufallsRaum);
+        testEbene1.raumEinsetzen(ZAHL_7, ZAHL_0, truhenRaum);
+        testEbene1.raumEinsetzen(ZAHL_8, ZAHL_0, gegnerRaum);
+        testEbene1.raumEinsetzen(ZAHL_8, ZAHL_1, treppenRaum);
 
         System.out.println(testEbene1.toString());
 
@@ -118,16 +113,16 @@ public class EbeneTest
         EbeneController.initSpielerInEbene(spielfigur, testEbene2);
 
         System.out.println(testEbene2.toString());
-        System.out.println(testEbene2.getRaumAnPosition(ZAHL_4,ZAHL_4).getSpielfigur());
+        System.out.println(testEbene2.getRaumAnPosition(ZAHL_4, ZAHL_4).getSpielfigur());
 
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_4,ZAHL_3,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_4,ZAHL_2,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_4,ZAHL_1,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_4,ZAHL_0,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_5,ZAHL_0,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_6,ZAHL_0,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_7,ZAHL_0,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_8,ZAHL_0,spielfigur,spielStand);
-        SpielfigurEbeneController.bewegen(testEbene2,ZAHL_8,ZAHL_1,spielfigur,spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_4, ZAHL_3, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_4, ZAHL_2, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_4, ZAHL_1, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_4, ZAHL_0, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_5, ZAHL_0, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_6, ZAHL_0, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_7, ZAHL_0, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_8, ZAHL_0, spielfigur, spielStand);
+        SpielfigurEbeneController.bewegen(testEbene2, ZAHL_8, ZAHL_1, spielfigur, spielStand);
     }
 }
---
+++ src/resources/StringsEreignisse.java
@@ -0,0 +1,30 @@
+package resources;
+
+public interface StringsEreignisse
+{
+    //Namen der Ereignisse
+
+    String START_RAUM_NAME = "Startpunkt.";
+    String HAENDLER_NAME = "Der Spielplatz.";
+    String TEMPEL_NAME = "Die Nachbarschaft.";
+    String SCHMIED_NAME = "Die Schule.";
+    String HEILER_NAME = "Mutti.";
+    String GEGNER_NAME = "Gegner.";
+    String TRUHE_NAME = "Truhe.";
+    String ZUFALLS_EREIGNIS_NAME = "Zufaelliges Ereignis.";
+    String LEERER_RAUM_NAME = "Leere Straße.";
+    String TREPPE_NAME = "Die Bushaltestelle.";
+
+    //Beschreibungen der Ereignisse
+
+    String START_RAUM_BESCHREIBUNG = "Hier bin ich ausgestiegen, ich sollte mich in der Gegend mal umsehen.";
+    String HAENDLER_BESCHREIBUNG = "Der Spielplatz! Ein großartiger Ort um neue Freunde zu finden.";
+    String TEMPEL_BESCHREIBUNG = "Wenn ich einen meiner Freunde loswerden will, sollte ich bei seinen Eltern petzen gehen.";
+    String SCHMIED_BESCHREIBUNG = "In der Schule kann man sicher viel Neues lernen.";
+    String HEILER_BESCHREIBUNG = "Wenn ich Schmerzen hab sollte ich zu Mutti gehen. Danach gehts es mir immer besser.";
+    String GEGNER_BESCHREIBUNG = "Ein Gegner! Sollte ich ihn bekaempfen oder doch versuchen ihn zu meiden?";
+    String TRUHE_BESCHREIBUNG = "Eine Truhe, was da wohl drin ist?";
+    String ZUFALLS_EREIGNIS_BESCHREIBUNG = "Ich kann nicht erkennen was vor mir ist. Ich muss näher rangehen...";
+    String LEERER_RAUM_BESCHREIBUNG = "Hier ist absolut gar nichts... wie öde.";
+    String TREPPE_BESCHREIBUNG = "Der naechste Bus kommt gleich. Wenn ich weiter will sollte ich mitfahren.";
+}
