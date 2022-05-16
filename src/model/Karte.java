47583e87baf5bf4837b5001701d6ce4e17b14b07
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-05-16 10:59:40 +0200

    Dokumentation in der Klasse Karte geschrieben

--- src/model/Karte.java
+++ src/model/Karte.java
@@ -26,9 +26,11 @@ public abstract class Karte
         instanzZaehler++;
     }
 
+    /**
+     * Leerer Konstruktor der Klasse Karte.
+     */
     public Karte ()
     {
-
     }
 
     /**
@@ -50,16 +52,28 @@ public abstract class Karte
         return id;
     }
 
+    /**
+     * Methode um den String des Attributes name zu bekommen.
+     * @return gibt den String des Attributes name wieder.
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Mehtode um den Int-Wert des Attributes level zu bekommen.
+     * @return gibt den Int-Wert des Attributes level wieder.
+     */
     public int getLevel()
     {
         return level;
     }
 
+    /**
+     * Methode um einen Int-Wert in das Attribut level zu setzen.
+     * @param level Int-Wert, der in das Attribut level gesetzt werden soll.
+     */
     public void setLevel(int level)
     {
         this.level = level;
