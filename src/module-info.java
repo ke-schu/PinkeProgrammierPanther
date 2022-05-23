ba4efa0133a27feab16ba85e46ec07a12a01a2f5
Adrian <aruholt@stud.hs-bremen.de>
2022-05-23 16:00:41 +0200

    JavaFX fully added AR

---
+++ src/module-info.java
@@ -0,0 +1,19 @@
+module PinkeProgrammierPanther {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.fxml;
+    requires com.google.gson;
+    //requires com.google.gson;
+
+    opens gui;
+    opens control;
+    opens exceptions;
+    opens io;
+    opens model;
+    opens resources;
+    opens model.ereignisse;
+}
