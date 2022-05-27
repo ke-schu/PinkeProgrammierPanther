8e38f3cb6efd064c3845599d565a41e4e86298dd
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-05-27 17:42:27 +0200

    versucht Logo als Icon einzusetzen

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,8 +14,12 @@ public class Gui extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("xml/Hauptmenue.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        //Image logo = new Image("C:\\Users\\hendr\\Documents\\GitHub\\PinkeProgrammierPanther\\src\\gui\\bilder\\Logo.png");
+        // funktioniert aber nur mit dem Verweis auf meinem Rechner, andere Verweise innerhalb des Projektes hab ich so erstmal
+        // nicht zum laufen bekommen.
         stage.setTitle("DungeonDing");
         stage.setScene(scene);
+        //stage.getIcons().add(logo); Gehoert zum Logo
         stage.show();
     }
 
