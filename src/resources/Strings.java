f133518f0f1a1244184d937b32b9f645bb897006
Keno Schumann <kejuhi79@gmail.com>
2022-05-28 17:59:45 +0200

    Unnötigen String entfernt

--- src/resources/Strings.java
+++ src/resources/Strings.java
@@ -61,5 +61,4 @@ public interface Strings
             "Die Karte konnte nicht verbessert werden.";
     String KARTEN_DECK_ZU_KLEIN_INFO =
             "Das Kartendeck enthält zu wenige Karten für die Operation.";
-    String JSON_FORMAT_FEHLERHAFT_INFO = "Fehlerhafte Json Formatierung";
 }
