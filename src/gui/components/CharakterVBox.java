3743e8976cb2a2517f41a30c6fc04d7df5bcf919
Keno Schumann <kejuhi79@gmail.com>
2022-06-13 15:21:51 +0200

    CharakterVBOX angepasst

--- src/gui/components/CharakterVBox.java
+++ src/gui/components/CharakterVBox.java
@@ -10,8 +10,8 @@ import model.Charakter;
 import static gui.GuiKonstanten.*;
 
 /**
- * Modelliert eine VBox mit eigenen Priority-Klassen um die Charaktere
- * in der Szene darstellen zu können.
+ * Modelliert eine VBox mit eigenen Priority-Klassen um die Charaktere in der
+ * Szene darstellen zu können.
  */
 public class CharakterVBox extends VBox
 {
@@ -22,51 +22,50 @@ public class CharakterVBox extends VBox
     private final static String PSEUDO_CLASS_FREIGESCHALTET = "nicht-freigeschaltet";
 
     /**
-     * Konstruiert die CharakterVBox und fügt Listener für die benötigten Priority-Klassen hinzu.
+     * Konstruiert die CharakterVBox und fügt Listener für die benötigten
+     * Priority-Klassen hinzu.
      */
     public CharakterVBox(Charakter c)
     {
         getStyleClass().add(STYLE_CLASS);
 
+        freigeschaltet = new SimpleBooleanProperty(c.getFreigeschaltet());
+        freigeschaltet.addListener(e ->
+                                   {
+                                       this.setFreigeschaltet(
+                                               freigeschaltet.get());
+                                       initialisiere(c);
+                                   });
+
         gewaehlt = new SimpleBooleanProperty(false);
-        gewaehlt.addListener(e -> {
-            pseudoClassStateChanged(PseudoClass.getPseudoClass(PSEUDO_CLASS_GEWAEHLT), gewaehlt.get());
-        });
+        gewaehlt.addListener(e -> pseudoClassStateChanged(
+                                             PseudoClass.getPseudoClass(
+                                                     PSEUDO_CLASS_GEWAEHLT),
+                                             gewaehlt.get()));
 
-        /*
-        Der Zustand, ob eine CharakterVBox freigeschaltet ist, ist in dieser
-        Klasse invertiert implementiert, damit in der CSS-Datei die Pseudo-Klasse
-        "nicht-freigeschaltet" angewendet werden kann (siehe Getter & Setter)
-         */
-        freigeschaltet = new SimpleBooleanProperty(false);
-        freigeschaltet.addListener(e -> {
-            pseudoClassStateChanged(PseudoClass.getPseudoClass(PSEUDO_CLASS_FREIGESCHALTET), freigeschaltet.get());
-        });
-
-        BooleanProperty freigeschaltet = new SimpleBooleanProperty(c.getFreigeschaltet());
-        updateFreigeschaltet(c, freigeschaltet.get());
-        freigeschaltet.addListener((observableValue, aBoolean, t1) ->
-                                           updateFreigeschaltet(c, freigeschaltet.get()));
+        initialisiere(c);
     }
 
-    private void updateFreigeschaltet(Charakter c, boolean b)
+    private void initialisiere(Charakter c)
     {
+        pseudoClassStateChanged(
+                PseudoClass.getPseudoClass(PSEUDO_CLASS_FREIGESCHALTET),
+                !freigeschaltet.get());
         this.getChildren().clear();
 
         Label name = new Label(c.getName());
         name.setId(STYLE_CHARAKTER_NAME);
         this.getChildren().add(name);
 
-        this.setFreigeschaltet(b);
-        if(!b)
+        if (!freigeschaltet.get())
         {
             this.getChildren()
-             .add(new Label(String.format(CHARAKTER_KAUFEN, c.getFreischaltgebuehr())));
-        }
-        else
+                .add(new Label(String.format(CHARAKTER_KAUFEN,
+                                             c.getFreischaltgebuehr())));
+        } else
         {
             this.getChildren()
-             .add(new Label(SCHON_FREIGESCHALTET));
+                .add(new Label(SCHON_FREIGESCHALTET));
         }
     }
 
@@ -74,7 +73,8 @@ public class CharakterVBox extends VBox
      * Gibt zurück, ob ein Charakter aktuell gewählt ist.
      * @return true, wenn er gewaehlt ist.
      */
-    public boolean istGewaehlt() {
+    public boolean istGewaehlt()
+    {
         return gewaehlt.get();
     }
 
@@ -82,7 +82,8 @@ public class CharakterVBox extends VBox
      * Setzt den Zustand gewaehlt des Charakters.
      * @param gewaehlt true, wenn gewaehlt.
      */
-    public void setGewaehlt(boolean gewaehlt) {
+    public void setGewaehlt(boolean gewaehlt)
+    {
         this.gewaehlt.set(gewaehlt);
     }
 
@@ -90,8 +91,9 @@ public class CharakterVBox extends VBox
      * Gibt zurück, ob ein Charakter freigeschaltet ist.
      * @return true, wenn er freigeschaltet ist.
      */
-    public boolean istFreigeschaltet() {
-        return !freigeschaltet.get();
+    public boolean istFreigeschaltet()
+    {
+        return freigeschaltet.get();
     }
 
     /**
@@ -100,6 +102,6 @@ public class CharakterVBox extends VBox
      */
     public void setFreigeschaltet(boolean freigeschaltet)
     {
-        this.freigeschaltet.set(!freigeschaltet);
+        this.freigeschaltet.set(freigeschaltet);
     }
 }
