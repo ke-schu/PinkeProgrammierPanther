7bed05be43596a938b1a157eb464bf91379f8d5c
Keno Schumann <kejuhi79@gmail.com>
2022-06-13 18:15:48 +0200

    Gui.java gelöscht

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,11 +1,22 @@
 package control;
 
 import control.test.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import view.mp3Controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import static resources.GuiKonstanten.*;
 
 /**
  * In dieser Klasse startet das Programm.
  */
-public class Main
+public class Main extends Application
 {
     /**
      * Diese main-Methode wird beim Start des Programms aufgerufen.
@@ -13,7 +24,27 @@ public class Main
      */
     public static void main(String[] args)
     {
-        teste();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        File f = new File(HAUPTMENUE_PFAD);
+        FXMLLoader fxmlLoader = new FXMLLoader(f.toURI().toURL());
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle(SPIELTITEL);
+        stage.setScene(scene);
+        stage.setMaxHeight(AUFLOESUNG_HOEHE_HD); //Aufloesung bei Start
+        stage.setMinHeight(AUFLOESUNG_HOEHE_HD);
+        stage.setMaxWidth(AUFLOESUNG_BREITE_HD);
+        stage.setMinWidth(AUFLOESUNG_BREITE_HD);
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(ICON.getAbsolutePath()));
+        stage.show();
+        mp3Controller.spieleHintergrundmusik();
     }
 
     /**
