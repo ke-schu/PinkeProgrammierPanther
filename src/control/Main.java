af6722c02547ae4966b63a2220c71b01ed793bd0
Keno Schumann <kejuhi79@gmail.com>
2022-06-13 18:23:08 +0200

    Main TryCatch und Doku

--- src/control/Main.java
+++ src/control/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import utility.KonsolenIO;
 import view.mp3Controller;
 
 import java.io.File;
@@ -27,23 +28,33 @@ public class Main extends Application
         launch();
     }
 
+    /**
+     * Mit dieser Methode wird die GUI in einem neuen Window geoeffnet.
+     * @param stage die Buehne des Programms
+     */
     @Override
-    public void start(Stage stage) throws IOException
+    public void start(Stage stage)
     {
         File f = new File(HAUPTMENUE_PFAD);
-        FXMLLoader fxmlLoader = new FXMLLoader(f.toURI().toURL());
-
-        Scene scene = new Scene(fxmlLoader.load());
-
-        stage.setTitle(SPIELTITEL);
-        stage.setScene(scene);
-        stage.setMaxHeight(AUFLOESUNG_HOEHE_HD); //Aufloesung bei Start
-        stage.setMinHeight(AUFLOESUNG_HOEHE_HD);
-        stage.setMaxWidth(AUFLOESUNG_BREITE_HD);
-        stage.setMinWidth(AUFLOESUNG_BREITE_HD);
-        stage.setResizable(false);
-        stage.getIcons().add(new Image(ICON.getAbsolutePath()));
-        stage.show();
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(f.toURI().toURL());
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle(SPIELTITEL);
+            stage.setScene(scene);
+            // Setze Aufloesung beim Starten
+            stage.setMaxHeight(AUFLOESUNG_HOEHE_HD);
+            stage.setMinHeight(AUFLOESUNG_HOEHE_HD);
+            stage.setMaxWidth(AUFLOESUNG_BREITE_HD);
+            stage.setMinWidth(AUFLOESUNG_BREITE_HD);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image(ICON.getAbsolutePath()));
+            stage.show();
+        }
+        catch (IOException e)
+        {
+            KonsolenIO.ausgeben(e.getMessage());
+        }
         mp3Controller.spieleHintergrundmusik();
     }
 
