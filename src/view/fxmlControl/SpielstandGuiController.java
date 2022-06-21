175fc45d133933451fc7636c874b73331dcd11d2
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-06-21 21:37:36 +0200

    Angefangen Spielstand Gui zu implementieren

--- src/view/fxmlControl/SpielstandGuiController.java
+++ src/view/fxmlControl/SpielstandGuiController.java
@@ -1,12 +1,43 @@
 package view.fxmlControl;
 
+import exceptions.JsonNichtLesbarException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import model.SpielStand;
+import utility.KartenDeckIO;
+import utility.SpielStandIO;
 
-public class SpielstandGuiController extends GuiController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static resources.Strings.SPIEL_DECK_SPIELER_PFAD;
+
+public class SpielstandGuiController extends GuiController implements
+                                                           Initializable
 {
+    @FXML Label goldLabel;
+    @FXML Label levelLabel;
+    @FXML Label decknameLabel;
+    @FXML ImageView deckBild;
+    @Override public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        try
+        {
+            SpielStand spielstand = SpielStandIO.leseDatei();
+            this.goldLabel.setText(Integer.toString(spielstand.getGold()));
+            this.levelLabel.setText(Integer.toString(spielstand.getSpieler().getLevel()));
+            this.decknameLabel.setText(KartenDeckIO.leseDatei(SPIEL_DECK_SPIELER_PFAD).getDeckBezeichnung());
+        }
+        catch (JsonNichtLesbarException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML public void schliesseSpielstand(ActionEvent event)
     {
