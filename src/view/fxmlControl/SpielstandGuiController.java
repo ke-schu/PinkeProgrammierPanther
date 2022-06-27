f5ea8758668644039cc002d0240e99f5fc4e5369
boeni99 <104452039+boeni99@users.noreply.github.com>
2022-06-27 11:51:10 +0200

    Spielstand anzeigen Bild eingefügt

--- src/view/fxmlControl/SpielstandGuiController.java
+++ src/view/fxmlControl/SpielstandGuiController.java
@@ -6,16 +6,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import model.Charakter;
 import model.SpielStand;
 import utility.KartenDeckIO;
 import utility.SpielStandIO;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import static resources.Strings.SPIEL_DECK_SPIELER_PFAD;
+import static resources.StringsGUI.BILDER_PFAD;
+import static resources.StringsGUI.PNG_DATEI_ENDUNG;
 
 public class SpielstandGuiController extends GuiController implements
                                                            Initializable
@@ -32,6 +37,8 @@ public class SpielstandGuiController extends GuiController implements
             this.goldLabel.setText(Integer.toString(spielstand.getGold()));
             this.levelLabel.setText(Integer.toString(spielstand.getSpieler().getLevel()));
             this.decknameLabel.setText(KartenDeckIO.leseDatei(SPIEL_DECK_SPIELER_PFAD).getDeckBezeichnung());
+            File charakterbild = new File(BILDER_PFAD + spielstand.getSpieler().getName() + PNG_DATEI_ENDUNG);
+            this.deckBild.setImage(new Image(charakterbild.getAbsolutePath()));
         }
         catch (JsonNichtLesbarException e)
         {
