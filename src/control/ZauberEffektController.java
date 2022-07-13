6c95b11204af3b8665fc8ff3d49ec6c311eab33c
DieBlaueElise <7crazyhit@gmail.com>
2022-07-13 17:44:18 +0200

    hier du socke

--- src/control/ZauberEffektController.java
+++ src/control/ZauberEffektController.java
@@ -3,6 +3,7 @@ package control;
 import model.*;
 
 import static control.EinheitenController.verursacheSchaden;
+import static resources.KonstantenGUI.*;
 import static resources.StringsEreignisse.SCHADEN_WURFSPEER;
 
 /**
@@ -20,7 +21,7 @@ public class ZauberEffektController
      * @param spielerDeck Das Deck des Players.
      * @param masterDeck  Das Deck des DungeonMaster.
      */
-    public static void zauberKarteAusspielen(KarteZauber ausloeser,
+    public static int zauberKarteAusspielen(KarteZauber ausloeser,
                                              KarteEinheit ziel,
                                              KartenHand hand,int handposition,
                                              SpielFeld feld,
@@ -29,55 +30,68 @@ public class ZauberEffektController
 
     {
         hand.setElement(handposition, null);
+        int rueckmeldung = RUECKMELDUNG_ERFOLGLOS;
 
         switch (ausloeser.getZeffekt())
         {
             case WURFSPEER:
-                wurfspeer(ausloeser ,ziel, feld, spielerDeck, masterDeck);
+                rueckmeldung = wurfspeer(ausloeser ,ziel, feld, spielerDeck, masterDeck);
                 break;
             case HEILUNG:
-                heilen(ausloeser, ziel, feld, spielerDeck, masterDeck);
+                rueckmeldung = heilen(ausloeser, ziel, feld, spielerDeck, masterDeck);
+                break;
             case VERSTAERKEN:
-                verstaerken(ausloeser, ziel, feld, spielerDeck, masterDeck);
+                rueckmeldung = verstaerken(ausloeser, ziel, feld, spielerDeck, masterDeck);
+                break;
             default:
-                return;
+
         }
+        return rueckmeldung;
     }
 
     /**
      * Diese Methode wird zu einem späteren Zeitpunkt ausgefuehrt.
      */
-    private static void wurfspeer(KarteZauber angreifer, KarteEinheit verteidiger, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
+    private static int wurfspeer(KarteZauber angreifer, KarteEinheit verteidiger, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
     {
         if(angreifer.getFreundlich()!= verteidiger.getFreundlich())
         {
             verteidiger.schadenNehmen(angreifer.getMacht());
-            RundenController.feldplatzAufraumen(feld,
+            boolean rückmeldung =RundenController.feldplatzAufraumen(feld,
                     spielerDeck,
                     masterdeck,
                     verteidiger.getPositionX(),
                     verteidiger.getPositionY());
+            if (rückmeldung)
+            {
+                return RUECKMELDUNG_GESTORBEN ;
+            }
+            else return RUECKMELDUNG_SCHADEN;
         }
-
+        return RUECKMELDUNG_ERFOLGLOS;
     }
 
-    private static void heilen(KarteZauber angreifer, KarteEinheit ziel, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
+    private static int heilen(KarteZauber angreifer, KarteEinheit ziel, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
     {
         if(angreifer.getFreundlich() == ziel.getFreundlich())
         {
             ziel.heilen(angreifer.getMacht());
             RundenController.feldplatzAufraumen(feld, spielerDeck, masterdeck,
                     ziel.getPositionX(), ziel.getPositionY());
+            return RUECKMELDUNG_SCHADEN;
         }
+        return RUECKMELDUNG_ERFOLGLOS;
     }
 
-    private static void verstaerken(KarteZauber angreifer, KarteEinheit ziel, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
+    private static int verstaerken(KarteZauber angreifer, KarteEinheit ziel, SpielFeld feld, KartenDeck spielerDeck, KartenDeck masterdeck)
     {
         if(angreifer.getFreundlich() == ziel.getFreundlich())
         {
             ziel.angriffErhoehen(angreifer.getMacht());
             RundenController.feldplatzAufraumen(feld, spielerDeck, masterdeck,
                     ziel.getPositionX(), ziel.getPositionY());
+            return RUECKMELDUNG_SCHADEN;
         }
+        return RUECKMELDUNG_ERFOLGLOS;
     }
 }
